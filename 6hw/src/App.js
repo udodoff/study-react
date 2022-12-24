1441805c38f3035e9aaf90fee4ccd88c56c5c530
Ruslan Udodov <udodovr03@gmail.com>
2022-12-24 17:03:00 +0300

    6 hw

---
+++ 6hw/src/App.js
@@ -0,0 +1,45 @@
+import {Route, Routes} from 'react-router-dom'
+import { useState } from "react"
+import { Provider } from "react-redux"
+
+import { Header } from "./components/Header"
+import { MainPage } from "./pages/MainPage"
+import { ProfilePage } from "./pages/ProfilePage"
+import { ChatsPage } from "./pages/ChatsPage"
+import { ChatList } from "./components/ChatList"
+import { defaultContext, ThemeContext } from "./utils/ThemeContext" 
+import { store } from "./store"
+
+export function App(){
+    const [theme, setTheme] = useState(defaultContext.theme)
+    
+    const toggleTheme = () =>{
+      setTheme(theme === 'light' ? 'dark' : 'light')
+    }
+
+    return(
+        <>
+            <Provider store={store}>
+              <ThemeContext.Provider value={{
+                theme, toggleTheme
+              }}>
+                <Routes>
+                    <Route path="/" element={<Header/>}>
+                        <Route index element={<MainPage/>}/>
+                        <Route path="profile" element={<ProfilePage/>}/>
+                        <Route path="chats">
+                            <Route index element={<ChatList  />} />
+                            <Route 
+                                path=":chatId" 
+                                element={<ChatsPage 
+                                 />} 
+                            />
+                        </Route>
+                    </Route>
+                    <Route path="*" element={<h2>404 Page not FOUND</h2>} />
+                </Routes>
+              </ThemeContext.Provider>
+            </Provider>
+        </>
+    )
+}
